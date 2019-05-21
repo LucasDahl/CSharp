52acb49b999ca8b7fbff5f8f66a1cf018184bf33
Lucas Dahl <lucasdahl425@yahoo.com>
2019-05-21 07:02:44 -0700

    Add files via upload
    A script ot handle spawning for a zombie game made in unity.

---
+++ SpawnHandler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnHandler : MonoBehaviour {
+
+	[SerializeField] private GameObject [] spawnObjects;
+	[SerializeField] private int amount;
+	
+
+	private void Start() {
+
+		Spawn();
+
+	}
+
+	private void Spawn() {
+
+		Transform tempTransform = gameObject.transform;
+
+		for (var i = 0; i < amount; i++) {
+
+			var randomIndex = Random.Range(0, spawnObjects.Length);
+			Instantiate(spawnObjects[randomIndex], tempTransform.position, tempTransform.rotation);
+			tempTransform.Translate(new Vector3(2, 0, 0));
+
+		}
+
+		foreach (var item in spawnObjects) {
+
+			print(item);
+
+		}
+
+		var myInt = 5;
+		var myIntTwo = 5;
+
+		while(myInt < 10) {
+
+			myInt++;
+
+		}
+
+		do {
+			myIntTwo++;
+		} while (myIntTwo < 10);
+
+
+	}
+
+}
